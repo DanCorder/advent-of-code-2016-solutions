76ce203911d408325b950fa5e1a44aded1bc1770
Dan Corder <dev@dancorder.com>
2016-12-04 09:32:38 +0000

    Day 4 problem 1

--- Program.cs
+++ Program.cs
@@ -11,7 +11,8 @@ namespace AdventOfCode
             // Console.WriteLine(Day02.SolveProblem1());
             // Console.WriteLine(Day02.SolveProblem2());
             // Console.WriteLine(Day03.SolveProblem1());
-            Console.WriteLine(Day03.SolveProblem2());
+            // Console.WriteLine(Day03.SolveProblem2());
+            Console.WriteLine(Day04.SolveProblem1());
         }
     }
 }
