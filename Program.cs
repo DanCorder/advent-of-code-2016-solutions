86755750869c9f4ad46d3fcf81b25e67d8014045
Dan Corder <dev@dancorder.com>
2016-12-04 00:16:58 +0000

    Day 3 problem 1

--- Program.cs
+++ Program.cs
@@ -6,10 +6,11 @@ namespace AdventOfCode
     {
         public static void Main(string[] args)
         {
-            //Console.WriteLine(Day01.SolveProblem1());
-            //Console.WriteLine(Day01.SolveProblem2());
+            // Console.WriteLine(Day01.SolveProblem1());
+            // Console.WriteLine(Day01.SolveProblem2());
             // Console.WriteLine(Day02.SolveProblem1());
-            Console.WriteLine(Day02.SolveProblem2());
+            // Console.WriteLine(Day02.SolveProblem2());
+            Console.WriteLine(Day03.SolveProblem1());
         }
     }
 }
