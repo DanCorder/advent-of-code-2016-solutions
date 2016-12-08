1d096a7481b24c7c1161f4312ad631f0d87abf5e
Dan Corder <dev@dancorder.com>
2016-12-08 08:25:04 +0000

    Update template

--- Template.cs
+++ Template.cs
@@ -1,14 +1,18 @@
 namespace AdventOfCode
 {
+    using System.Linq;
+
     public class Template
     {
         public static string SolveProblem1()
         {
+            var instructions = TestProblemInput1.Split('\r', '\n').Where(i => !string.IsNullOrEmpty(i));
             return TestProblemInput1;
         }
 
         public static string SolveProblem2()
         {
+            var instructions = TestProblemInput2.Split('\r', '\n').Where(i => !string.IsNullOrEmpty(i));
             return TestProblemInput2;
         }
 
