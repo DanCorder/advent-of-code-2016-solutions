2cced7c2fabe5754d3a6ce97b7f5c156f24e7888
Dan Corder <dev@dancorder.com>
2016-12-18 08:22:04 +0000

    Day 18 problem 1

---
+++ Day18.cs
@@ -0,0 +1,74 @@
+namespace AdventOfCode
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+
+    public class Day18
+    {
+        public static int SolveProblem1()
+        {
+            Console.WriteLine("Start: " + DateTime.Now);
+            //var input = ".^^.^.^^^^";
+            //var numberOfRows = 10;
+            var input = ".^^^.^.^^^.^.......^^.^^^^.^^^^..^^^^^.^.^^^..^^.^.^^..^.^..^^...^.^^.^^^...^^.^.^^^..^^^^.....^....";
+            var numberOfRows = 40;
+
+            var firstRowTraps = input.Select(c => c == '^').ToArray();
+
+            var floor = GetFloor(firstRowTraps, numberOfRows);
+
+            printFloor(floor);
+
+            var safeTiles = floor.Sum(r => r.Sum(t => t ? 0 : 1));
+
+            Console.WriteLine("End: " + DateTime.Now);
+            return safeTiles;
+        }
+
+        private static void printFloor(bool[][] floor)
+        {
+            foreach(var row in floor)
+            {
+                Console.WriteLine(new String(row.Select(t => t ? '^' : '.').ToArray()));
+            }
+        }
+
+        private static bool[][] GetFloor(bool[] firstRowTraps, int numberOfRows)
+        {
+            var ret = new List<bool[]>() {firstRowTraps};
+
+            while (ret.Count < numberOfRows)
+            {
+                ret.Add(GetNextRow(ret[ret.Count - 1]));
+            }
+
+            return ret.ToArray();
+        }
+
+        private static bool[] GetNextRow(bool[] previousRow)
+        {
+            var paddedRow = previousRow.Prepend(false).Append(false).ToArray();
+            var paddedNextRow = paddedRow.Select((t,i) => (i > 0 && i < paddedRow.Length - 1) && (
+                (paddedRow[i-1] && paddedRow[i] && !paddedRow[i+1]) ||
+                (!paddedRow[i-1] && paddedRow[i] && paddedRow[i+1]) ||
+                (paddedRow[i-1] && !paddedRow[i] && !paddedRow[i+1]) ||
+            (!paddedRow[i-1] && !paddedRow[i] && paddedRow[i+1])));
+
+            return paddedNextRow.Skip(1).Take(previousRow.Length).ToArray();
+        }
+
+        public static string SolveProblem2()
+        {
+            Console.WriteLine("Start: " + DateTime.Now);
+            var instructions = TestProblemInput2.SplitToLines();
+
+            Console.WriteLine("End: " + DateTime.Now);
+            return TestProblemInput2;
+        }
+
+        private static readonly string TestProblemInput1 = @"qq";
+        private static readonly string TestProblemInput2 = @"qq";
+        // private static readonly string ProblemInput = @"qq";
+    }
+}
