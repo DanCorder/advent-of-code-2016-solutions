d860787c4607255880a52d88a04eae57bba9ba5c
Dan Corder <dev@dancorder.com>
2016-12-19 08:22:21 +0000

    Day 19 problem 2

--- Day19.cs
+++ Day19.cs
@@ -6,7 +6,7 @@ namespace AdventOfCode
 
     public class Day19
     {
-        public static int SolveProblem1()
+        public static int SolveProblem1() // 1816277
         {
             Console.WriteLine("Start: " + DateTime.Now);
             var elvesList = Enumerable.Range(1, 3005290).ToList();
@@ -26,11 +26,11 @@ namespace AdventOfCode
                 if (currentElf == null)
                     currentElf = elves.First;
 
-                if (elves.Count % 10000 == 0)
-                {
-                    Console.WriteLine("Time: " + DateTime.Now);
-                    Console.WriteLine("Elves left: " + elves.Count);
-                }
+                // if (elves.Count % 10000 == 0)
+                // {
+                //     Console.WriteLine("Time: " + DateTime.Now);
+                //     Console.WriteLine("Elves left: " + elves.Count);
+                // }
             }
 
 
@@ -38,14 +38,60 @@ namespace AdventOfCode
             return elves.First.Value;
         }
 
-        public static string SolveProblem2()
+        public static int SolveProblem2() // 1410967
         {
             Console.WriteLine("Start: " + DateTime.Now);
-            var instructions = TestProblemInput2.SplitToLines();
+            var elvesList = Enumerable.Range(1, 3005290).ToList();
+            // var elvesList = Enumerable.Range(1, 5).ToList();
+            var elves = new LinkedList<int>(elvesList);
+
+            var currentElf = elves.First;
+            var currentSteal = GetElfToStealFrom(elves, currentElf);
+
+            while (elves.Count > 1)
+            {
+                var nextSteal = currentSteal.Next;
+                if (nextSteal == null)
+                    nextSteal = elves.First;
+                if (elves.Count % 2 == 1)
+                {
+                    nextSteal = nextSteal.Next;
+                    if (nextSteal == null)
+                        nextSteal = elves.First;
+                }
+
+                elves.Remove(currentSteal);
+
+                currentElf = currentElf.Next;
+                if (currentElf == null)
+                    currentElf = elves.First;
+
+                currentSteal = nextSteal;
+
+                // if (elves.Count % 10000 == 0)
+                // {
+                //     Console.WriteLine("Time: " + DateTime.Now);
+                //     Console.WriteLine("Elves left: " + elves.Count);
+                // }
+            }
+
 
             Console.WriteLine("End: " + DateTime.Now);
-            return TestProblemInput2;
+            return elves.First.Value;
         }
-        private static readonly string TestProblemInput2 = @"qq";
+
+        private static LinkedListNode<int> GetElfToStealFrom(LinkedList<int> elves, LinkedListNode<int> currentElf)
+        {
+            int offset = elves.Count / 2;
+            var ret = currentElf;
+
+            for (var i = 0; i < offset; i++)
+            {
+                ret = ret.Next;
+                if (ret == null)
+                    ret = elves.First;
+            }
+            return ret;
+        }
     }
 }
